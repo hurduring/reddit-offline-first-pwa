f2d164629f7d1b013e5052b1944d86efe84f9c1d
kon <idontwanttogoonthecart@gmail.com>
2016-12-20 19:41:55 +0400

    introducing redux

--- client/containers/home/index.js
+++ client/containers/home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    HOME123
+    HOME123fdas
   </div>
 );
 
--- client/index.js
+++ client/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
+import { Provider } from 'react-redux'
+import { createStore, compose } from 'redux';
+import rootReducer from './redux/rootReducer';
+
+const store = createStore(rootReducer, {}, compose(window.devToolsExtension()));
 
 const rootEl = document.getElementById('root');
 
 if (module.hot) {
   module.hot.accept('./routes', () => {
     ReactDOM.render(
-      <Routes />,
+      <Provider store={store}>
+        <Routes />
+      </Provider>,
       rootEl
     )
   });
+  module.hot.accepts('./redux/rootReducer', () => store.replaceReducer(rootReducer));
 }
 
 ReactDOM.render(
-  <Routes />,
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
   rootEl
 );
